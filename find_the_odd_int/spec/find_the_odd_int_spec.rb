7a04bd1a0713d8ede60db35121999d447ba194c9
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-20 16:07:29 +0100

    passing further tests

--- find_the_odd_int/spec/find_the_odd_int_spec.rb
+++ find_the_odd_int/spec/find_the_odd_int_spec.rb
@@ -8,4 +8,12 @@ describe '#find_it' do
   it 'returns expected output for array with multiple values' do
     expect(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])).to eq 5
   end
+
+  it 'returns expected out put for further test' do
+    expect(find_it([1,1,2,-2,5,2,4,4,-1,-2,5])).to eq -1
+  end
+
+  it 'returns expected out put for further test 2' do
+    expect(find_it([1,1,1,1,1,1,10,1,1,1,1])).to eq 10
+  end
 end
