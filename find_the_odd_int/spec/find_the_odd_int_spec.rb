6841975b0a38a8236d25911d0257bc20c5cadb0e
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-20 15:47:27 +0100

    second failing test

--- find_the_odd_int/spec/find_the_odd_int_spec.rb
+++ find_the_odd_int/spec/find_the_odd_int_spec.rb
@@ -4,4 +4,8 @@ describe '#find_it' do
   it 'returns expected output for array with one value' do
     expect(find_it([10])).to eq 10
   end
+
+  it 'returns expected output for array with multiple values' do
+    expect(find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])).to eq 5
+  end
 end
