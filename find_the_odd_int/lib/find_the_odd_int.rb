3aa8751d82e99b5850b7050898e48c6582e17dfa
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-20 16:10:58 +0100

    refactored for shorter answer

--- find_the_odd_int/lib/find_the_odd_int.rb
+++ find_the_odd_int/lib/find_the_odd_int.rb
@@ -1,9 +1,15 @@
 def find_it(seq)
-  seq.each do |x|
-    counter = 0
-    seq.each do |y|
-      counter += 1 if x == y
-    end
-    return x if counter.odd?
+  seq.uniq.each do |x|
+    return x if seq.count(x).odd?
   end
 end
+
+# def find_it(seq)
+#   seq.each do |x|
+#     counter = 0
+#     seq.each do |y|
+#       counter += 1 if x == y
+#     end
+#     return x if counter.odd?
+#   end
+# end
