52d74ee0eb9e9ba122e82b998c614af679090c71
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-27 16:20:00 +0100

    passed second test

--- pete,_the_baker/lib/pete_the_baker.rb
+++ pete,_the_baker/lib/pete_the_baker.rb
@@ -3,6 +3,10 @@ def cakes(recipe, available)
   while true
     fin = false
     recipe.each do |key, value|
+      unless available.key?(key)
+        fin = true
+        break
+      end
       available[key] -= recipe[key]
       fin = true if available[key] < 0
     end
--- pete,_the_baker/spec/pete_the_baker_spec.rb
+++ pete,_the_baker/spec/pete_the_baker_spec.rb
@@ -2,6 +2,10 @@ require 'pete_the_baker'
 
 describe '#cakes' do
   it 'returns two when there is enough for two cakes' do
-    expect(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200})).to eq 2
+    expect( cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}) ).to eq 2
+  end
+
+  it 'returns zero when there is no amount of a certain ingredient' do
+    expect( cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}) ).to eq 0
   end
 end
