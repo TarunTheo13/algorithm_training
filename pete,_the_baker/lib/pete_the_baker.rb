db2aa0249469575555ee2937596110b78886a74c
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-27 16:28:53 +0100

    refactored

--- pete,_the_baker/lib/pete_the_baker.rb
+++ pete,_the_baker/lib/pete_the_baker.rb
@@ -1,17 +1,3 @@
 def cakes(recipe, available)
-  answer = 0
-  while true
-    fin = false
-    recipe.each do |key, value|
-      unless available.key?(key)
-        fin = true
-        break
-      end
-      available[key] -= recipe[key]
-      fin = true if available[key] < 0
-    end
-    break if fin
-    answer += 1
-  end
-  return answer
+  recipe.collect { | key, value | available[key].to_i / value }.min
 end
