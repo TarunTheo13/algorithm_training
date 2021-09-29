7ab871b1bb10dc54f365863db61d10095897cc29
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-29 23:47:12 +0100

    passing second test

--- xmas_tree/spec/xmas_tree_spec.rb
+++ xmas_tree/spec/xmas_tree_spec.rb
@@ -4,4 +4,8 @@ describe '#xmas_tree' do
   it 'returns correct answer for a tree of size three' do
     expect(xmas_tree(3)).to eq ['__#__', '_###_', '#####', '__#__', '__#__']
   end
+
+  it 'returns correct answer for a tree of size six' do
+    expect(xmas_tree(6)).to eq ['_____#_____', '____###____', '___#####___', '__#######__', '_#########_', '###########', '_____#_____', '_____#_____']
+  end
 end
