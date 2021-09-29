5f67b8a4b9f91c433ad4d41f7099aa07ecfd9f0d
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-29 18:06:09 +0100

    passed first test

--- xmas_tree/lib/xmas_tree.rb
+++ xmas_tree/lib/xmas_tree.rb
@@ -1,3 +1,20 @@
 def xmas_tree(n)
+  ans = []
+  i = 1
 
+  while i <= n do
+    str = ""
+    (n-i).times { str += "_" }
+    ((2 * i) - 1).times { str += "#" }
+    (n-i).times { str += "_" }
+    ans << str
+    i += 1
+  end
+  
+  str = ""
+  (n-1).times { str += "_" }
+  str += "#"
+  (n-1).times { str += "_" }
+  2.times { ans << str}
+  return ans
 end
