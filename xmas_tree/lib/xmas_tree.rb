52a7fecb299f50299b3fef096cc8b40a2085fcf2
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-09-30 15:45:01 +0100

    refactored

--- xmas_tree/lib/xmas_tree.rb
+++ xmas_tree/lib/xmas_tree.rb
@@ -1,20 +1,5 @@
 def xmas_tree(n)
-  ans = []
-  i = 1
-
-  while i <= n do
-    str = ""
-    (n-i).times { str += "_" }
-    ((2 * i) - 1).times { str += "#" }
-    (n-i).times { str += "_" }
-    ans << str
-    i += 1
-  end
-  
-  str = ""
-  (n-1).times { str += "_" }
-  str += "#"
-  (n-1).times { str += "_" }
-  2.times { ans << str}
-  return ans
+  width = 2*n-1
+  tree = (1..n).map { |i| ('#' * (2*i-1)).center(width, '_') }
+  tree + [tree.first] + [tree.first]
 end
