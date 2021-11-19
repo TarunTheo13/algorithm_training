e4f8a97c1bc18b60f29af0e94d8eba93402a69a6
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-19 12:54:44 +0000

    passing tests equal to zero

--- invisible_cubes/lib/invisible_cubes.rb
+++ invisible_cubes/lib/invisible_cubes.rb
@@ -1,3 +1,3 @@
 def not_visible_cubes(n)
-  
+  0
 end
--- invisible_cubes/spec/invisible_cubes_spec.rb
+++ invisible_cubes/spec/invisible_cubes_spec.rb
@@ -1,7 +1,15 @@
 require 'invisible_cubes'
 
 describe '#not_visible_cubes' do
+  it 'returns expected value for cube of size zero' do
+    expect(not_visible_cubes(0)).to eq 0
+  end
+
   it 'returns expected value for cube of size one' do
     expect(not_visible_cubes(1)).to eq 0
+  end
+
+  it 'returns expected value for cube of size two' do
+    expect(not_visible_cubes(2)).to eq 0
   end
 end
