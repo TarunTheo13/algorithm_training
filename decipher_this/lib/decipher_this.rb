9c4cef95e978be295826ee24931ba32b67ca9eda
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-22 12:42:12 +0000

    passed second test

--- decipher_this/lib/decipher_this.rb
+++ decipher_this/lib/decipher_this.rb
@@ -1,3 +1,9 @@
 def decipher_this(string)
+  if string.length > 2
+    second = string[1]
+    string[1] = string[string.length-1]
+    string[string.length-1] = second
+  end
   string[0] = string[0].ord.to_s
+  return string
 end
--- decipher_this/spec/decipher_this_spec.rb
+++ decipher_this/spec/decipher_this_spec.rb
@@ -4,4 +4,8 @@ describe '#decipher_this' do
   it 'returns expected string for one letter' do
     expect(decipher_this('d')).to eq '100'
   end
+
+  it 'returns expected string for one word' do
+    expect(decipher_this('Hello')).to eq '72olle'
+  end
 end
