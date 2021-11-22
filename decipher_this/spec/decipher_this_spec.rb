7595043848031e07de99586086d8c0651049e923
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-22 13:26:03 +0000

    passing all tests

--- decipher_this/spec/decipher_this_spec.rb
+++ decipher_this/spec/decipher_this_spec.rb
@@ -12,4 +12,8 @@ describe '#decipher_this' do
   it 'returns expected string for multiple words' do
     expect(decipher_this('65 119esi 111dl 111lw 108dvei 105n 97n 111ka')).to eq 'A wise old owl lived in an oak'
   end
+
+  it 'returns expected string for a different sentence' do
+    expect(decipher_this('87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri')).to eq 'Why can we not all be like that wise old bird'
+  end
 end
