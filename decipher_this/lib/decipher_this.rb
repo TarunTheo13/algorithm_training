b604d5294bcdb0d75106945034ea73147028f531
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-22 13:43:28 +0000

    refactored

--- decipher_this/lib/decipher_this.rb
+++ decipher_this/lib/decipher_this.rb
@@ -1,21 +1,7 @@
 def decipher_this(string)
-  arr = string.split
-  arr2 = []
-  arr.each do |word|
-    char_code = []
-    word.split.each do |x| 
-      if x.to_i.is_a? Numeric
-        char_code.push(x)
-      end
-    end
-    word.insert(0, char_code[0].to_i.chr)
-    word.tr!("0-9", "")
-    if word.length > 2
-      second = word[1]
-      word[1] = word[word.length-1]
-      word[word.length-1] = second
-    end
-     arr2.push(word)
-  end
-  return arr2.join(' ')
+  string.split(' ').each do |word|
+    char_code = word.scan(/[0-9]/).join
+    word.gsub!(char_code, char_code.to_i.chr)
+    word[1], word[-1] = word[-1], word[1] if word.size > 2
+  end.join(' ')
 end
