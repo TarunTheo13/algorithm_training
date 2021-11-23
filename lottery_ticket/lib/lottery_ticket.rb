753c7b958a535342b82b2814852ca9d739705bae
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-23 12:16:16 +0000

    passing first two tests

--- lottery_ticket/lib/lottery_ticket.rb
+++ lottery_ticket/lib/lottery_ticket.rb
@@ -1,3 +1,13 @@
 def bingo(ticket, win)
+  counter = 0
+  ticket.each do |x|
+    x[0].split.each do |y|
+      if y.ord == x[1]
+        counter += 1
+        break
+      end
+    end
+  end
+  return 'Winner!' if counter >= win
   'Loser!'
 end
--- lottery_ticket/spec/lottery_ticket_spec.rb
+++ lottery_ticket/spec/lottery_ticket_spec.rb
@@ -4,4 +4,8 @@ describe '#bingo' do
   it 'returns loser when there are not enough character code matches' do
     expect(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 2)).to eq 'Loser!'
   end
+
+  it 'returns winner when there are enough matches' do
+    expect(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1)).to eq 'Winner!'
+  end
 end
