e102a1249045add04189fde62f1a334d1a934e0b
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-24 13:07:50 +0000

    refactored

--- encrypt_this/lib/encrypt_this.rb
+++ encrypt_this/lib/encrypt_this.rb
@@ -1,7 +1,6 @@
 def encrypt_this(string)
-  string = string.split.each do |word|
+  string.split.each do |word|
     word[1], word[-1] = word[-1], word[1] if word.length > 2
     word[0] = word[0].ord.to_s
-  end
-  string.join(' ')
+  end.join(' ')
 end
