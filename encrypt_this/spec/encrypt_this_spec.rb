556c5d3c9fae57cdc551063670b2bdb0e3a1c59a
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-24 12:59:38 +0000

    third failing test

--- encrypt_this/spec/encrypt_this_spec.rb
+++ encrypt_this/spec/encrypt_this_spec.rb
@@ -8,4 +8,8 @@ describe '#encrypt this' do
   it 'returns expected string for one word' do
     expect(encrypt_this('Hello')).to eq '72olle'
   end
+
+  it 'returns expected string for multiple words' do
+    expect(encrypt_this('A wise old owl lived in an oak')).to eq '65 119esi 111dl 111lw 108dvei 105n 97n 111ka'
+  end
 end
