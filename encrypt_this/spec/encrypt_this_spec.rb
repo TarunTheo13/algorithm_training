146c49433ae7d4061e3f244ae186bb4975c816fa
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-24 13:04:32 +0000

    passing all tests

--- encrypt_this/spec/encrypt_this_spec.rb
+++ encrypt_this/spec/encrypt_this_spec.rb
@@ -12,4 +12,8 @@ describe '#encrypt this' do
   it 'returns expected string for multiple words' do
     expect(encrypt_this('A wise old owl lived in an oak')).to eq '65 119esi 111dl 111lw 108dvei 105n 97n 111ka'
   end
+
+  it 'returns expected string for a different sentence' do
+    expect(encrypt_this('Why can we not all be like that wise old bird')).to eq '87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri'
+  end
 end
