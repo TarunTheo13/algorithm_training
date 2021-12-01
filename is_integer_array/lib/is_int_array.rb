8d672724691f8785da0b4f7d441d005a16eb203e
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-12-01 13:43:35 +0000

    refactored to one line answer

--- is_integer_array/lib/is_int_array.rb
+++ is_integer_array/lib/is_int_array.rb
@@ -1,5 +1,3 @@
 def is_int_array(arr)
-  return false if !(arr.is_a? Array)
-  arr.each {|x| return false if (!(x.is_a? Integer) && !(x.is_a? Float) || !(x % 1 == 0))}
-  true
+  arr.all? {|x| x % 1.0 == 0} rescue false
 end
