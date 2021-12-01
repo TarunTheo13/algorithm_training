8af543a6788a76711fbfbfb9ad293d1a5a00ac69
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-12-01 13:28:34 +0000

    third failing test

--- is_integer_array/spec/is_int_array_spec.rb
+++ is_integer_array/spec/is_int_array_spec.rb
@@ -8,4 +8,8 @@ describe '#is_int_array' do
   it 'returns true for array with only integers' do
     expect(is_int_array([1, 2, 3, 4])).to eq true
   end
-end 
+
+  it 'returns false for array with nil' do
+    expect(is_int_array([1, 2, nil])).to eq false
+  end
+end
