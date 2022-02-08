1b939d816bbac63cb05617801c44b20ba67071a3
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-02-08 17:30:13 +0000

    second failing test

--- tribonacci_sequence/spec/tribonacci_spec.rb
+++ tribonacci_sequence/spec/tribonacci_spec.rb
@@ -4,4 +4,8 @@ describe '#tribonacci' do
   it 'returns expected array for n=3' do
     expect(tribonacci([1,1,1], 3)). to eq [1,1,1]
   end
+
+  it 'returns expected array for n=2' do
+    expect(tribonacci([1,1,1], 2)). to eq [1,1]
+  end
 end
