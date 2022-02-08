ff0126379c61cf203d073c92140eb27ac0069312
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-02-08 17:59:52 +0000

    refactoring

--- tribonacci_sequence/lib/tribonacci.rb
+++ tribonacci_sequence/lib/tribonacci.rb
@@ -1,8 +1,6 @@
 def tribonacci(arr, n)
-  if n < 3
-    (3-n).times { arr.pop }
-  elsif n > 3
-    (n-3).times { arr.push(arr[arr.length-3]+arr[arr.length-2]+arr[arr.length-1]) }
+  if n > 3
+    (n-3).times { arr << (arr[arr.length-3]+arr[arr.length-2]+arr[arr.length-1]) }
   end
-  arr
+  arr.slice(0, n)
 end
