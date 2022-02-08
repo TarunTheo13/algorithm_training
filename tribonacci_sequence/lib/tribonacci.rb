ae6e31ebd43b2954c5f1a7188465b838074cbae3
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-02-08 17:51:02 +0000

    passed fifth test

--- tribonacci_sequence/lib/tribonacci.rb
+++ tribonacci_sequence/lib/tribonacci.rb
@@ -1,6 +1,8 @@
-def tribonacci(signature, n)
+def tribonacci(arr, n)
   if n < 3
-    (3-n).times { signature.pop }
+    (3-n).times { arr.pop }
+  elsif n > 3
+    arr.push(arr[0]+arr[1]+arr[2])
   end
-  signature
+  arr
 end
