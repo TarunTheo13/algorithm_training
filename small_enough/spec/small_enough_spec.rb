2e0f7e74d2dd3e91215f2464e27e683151a85486
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-02-14 12:13:35 +0000

    passing all tests

--- small_enough/spec/small_enough_spec.rb
+++ small_enough/spec/small_enough_spec.rb
@@ -8,4 +8,12 @@ describe '#small_enough' do
   it 'returns false for single value above limit' do
     expect(small_enough([5], 2)).to eq false
   end
+
+  it 'returns true for multiple values' do
+    expect(small_enough([101, 45, 75, 105, 99, 107], 107)).to eq true
+  end
+
+  it 'returns false for multiple values' do
+    expect(small_enough([101, 45, 75, 210, 99, 107], 107)).to eq false
+  end
 end
