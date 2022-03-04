bf97fdedbe17be08bcdce275f3c538efda5a69c2
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-03-04 13:02:15 +0000

    second failing test

--- list_filtering/spec/filter_list_spec.rb
+++ list_filtering/spec/filter_list_spec.rb
@@ -4,4 +4,8 @@ describe '#filter_list' do
   it 'returns expected answer for array of numbers' do
     expect(filter_list([1,2])).to eq [1,2]
   end
+
+  it 'returns expected answer for array of numbers and letters' do
+    expect(filter_list([1,2,'g','ha',7])).to eq [1,2,7]
+  end
 end
