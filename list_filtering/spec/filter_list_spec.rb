8e1c6954d9ab42b130fae0b29479e4b289c70393
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-03-04 13:09:20 +0000

    passing all tests

--- list_filtering/spec/filter_list_spec.rb
+++ list_filtering/spec/filter_list_spec.rb
@@ -8,4 +8,8 @@ describe '#filter_list' do
   it 'returns expected answer for array of numbers and letters' do
     expect(filter_list([1,2,'g','ha',7])).to eq [1,2,7]
   end
+
+  it 'returns expected answer for array of repeated numbers and letters' do
+    expect(filter_list([1,2,'aasf','1','123',123])).to eq [1,2,123]
+  end
 end
