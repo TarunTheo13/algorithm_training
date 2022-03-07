5cdedd66d7b9c615cfade40ae6777028e320a4c2
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-03-07 15:14:32 +0000

    third failing test

--- find_perfect_square/spec/find_next_square_spec.rb
+++ find_perfect_square/spec/find_next_square_spec.rb
@@ -8,4 +8,8 @@ describe '#find_next_square' do
   it 'returns next perfect square if parameter is a perfect square (second example)' do
     expect(find_next_square(625)).to eq 676
   end
+
+  it 'returns -1 if parameter is not a perfect square' do
+    expect(find_next_square(114)).to eq -1
+  end
 end
