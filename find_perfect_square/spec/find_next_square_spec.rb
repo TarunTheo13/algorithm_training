c8d816cd0f1f718c7dd361bd9dfc426ad13dcfa7
Tarun/Theo Weisz <theo.weisz@gmail.com>
2022-03-07 15:13:23 +0000

    passed second test

--- find_perfect_square/spec/find_next_square_spec.rb
+++ find_perfect_square/spec/find_next_square_spec.rb
@@ -4,4 +4,8 @@ describe '#find_next_square' do
   it 'returns next perfect square if parameter is a perfect square' do
     expect(find_next_square(1)).to eq 4
   end
+
+  it 'returns next perfect square if parameter is a perfect square (second example)' do
+    expect(find_next_square(625)).to eq 676
+  end
 end
